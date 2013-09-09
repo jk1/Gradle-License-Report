19cf493cd9ddaf5a5991b35db298708e85121339
Robert Fischer <robert.fischer@progress.com>
2013-09-09 15:38:50 -0400

    Need a file that includes the license plugin

---
+++ src/main/groovy/com/smokejumperit/gradle/report/DependencyLicensePlugin.java
@@ -0,0 +1,15 @@
+package com.smokejumperit.gradle.report;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+import com.google.common.collect.ImmutableMap;
+
+public class DependencyLicensePlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		project.task(ImmutableMap.of("class", DependencyLicenseReport.class),
+				"dependencyLicenseReport");
+	}
+}
