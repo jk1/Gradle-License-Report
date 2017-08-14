31b3188ad596f15fa0e1490926ea5cf0c242e240
Zohaib Khan <zohaib.khan@swisscom.com>
2017-08-14 08:44:27 +0200

    Add a text report renderer.

---
+++ src/main/groovy/com/github/jk1/license/render/TextReportRenderer.groovy
@@ -0,0 +1,106 @@
+package com.github.jk1.license.render
+
+import com.github.jk1.license.License
+import com.github.jk1.license.LicenseReportPlugin
+import com.github.jk1.license.ManifestData
+import com.github.jk1.license.ModuleData
+import com.github.jk1.license.PomData
+import com.github.jk1.license.ProjectData
+import org.gradle.api.Project
+
+class TextReportRenderer implements ReportRenderer{
+
+
+    private Project project
+    private LicenseReportPlugin.LicenseReportExtension config
+    private File output
+    private int counter
+
+    void render(ProjectData data) {
+        project = data.project
+        config = project.licenseReport
+        output = new File(config.outputDir, 'THIRD-PARTY-NOTICES.txt')
+        output.text = """
+Dependency License Report for $project.name
+
+Dependency License Report for $project.name ${if (!'unspecified'.equals(project.version)) project.version else ''}
+
+"""
+        printDependencies(data)
+        output << """
+This report was generated at ${new Date()}.
+
+"""
+    }
+
+    private void printDependencies(ProjectData data) {
+        data.allDependencies.sort().each {
+            printDependency(it)
+        }
+    }
+
+    private String printDependency(ModuleData data) {
+        boolean projectUrlDone = false
+        output << "${++counter}."
+        if (data.group) output << " Group: $data.group "
+        if (data.name) output << " Name: $data.name "
+        if (data.version) output << " Version: $data.version\n\n"
+
+        if (data.poms.isEmpty() && data.manifests.isEmpty()) {
+            output << "No license information found\n\n"
+            return
+        }
+
+        if (!data.manifests.isEmpty() && !data.poms.isEmpty()) {
+            ManifestData manifest = data.manifests.first()
+            PomData pomData = data.poms.first()
+            if (manifest.url && pomData.projectUrl && manifest.url == pomData.projectUrl) {
+                output << "Project URL: $manifest.url\n\n"
+                projectUrlDone = true
+            }
+        }
+
+        if (!data.manifests.isEmpty()) {
+            ManifestData manifest = data.manifests.first()
+            if (manifest.url && !projectUrlDone) {
+                output << "Manifest Project URL: $manifest.url\n\n"
+            }
+            if (manifest.license) {
+                if (manifest.license.startsWith("http")) {
+                    output << "Manifest license URL: $manifest.license\n\n"
+                } else if (manifest.hasPackagedLicense) {
+                    output << "Packaged License File: $manifest.license\n\n"
+                } else {
+                    output << "Manifest License: $manifest.license (Not packaged)\n\n"
+                }
+            }
+        }
+
+        if (!data.poms.isEmpty()) {
+            PomData pomData = data.poms.first()
+            if (pomData.projectUrl && !projectUrlDone) {
+                output << "POM Project URL: $pomData.projectUrl\n\n"
+            }
+            if (pomData.licenses) {
+                pomData.licenses.each { License license ->
+                    output << "POM License: $license.name"
+                    if (license.url) {
+                        if (license.url.startsWith("http")) {
+                            output << " - $license.url\n\n"
+                        } else if (false) {
+                            output << "Packaged License File: $license.url\n\n"
+                        } else {
+                            output << "License: $license.url\n\n"
+                        }
+                    }
+                }
+            }
+        }
+        if (!data.licenseFiles.isEmpty() && !data.licenseFiles.first().files.isEmpty()) {
+            output << 'Embedded license: '
+            output << "\n\n"
+            output << data.licenseFiles.first().files.collect({ "                    ****************************************                    \n\n" + new File("$config.outputDir/$it").text + "\n"}).join('')
+        }
+        output << "--------------------------------------------------------------------------------\n\n"
+    }
+}
