94888c6f8acefdf0c40bda431ccd172f6fd28623
jk1 <eugenbox@gmail.com>
2017-09-30 10:26:50 +0300

    #30 afterEvaluate build hook removed

--- src/main/groovy/com/github/jk1/license/task/ReportTask.groovy
+++ src/main/groovy/com/github/jk1/license/task/ReportTask.groovy
@@ -4,20 +4,25 @@ import com.github.jk1.license.LicenseReportPlugin.LicenseReportExtension
 import com.github.jk1.license.ProjectData
 import com.github.jk1.license.reader.ProjectReader
 import org.gradle.api.DefaultTask
-import org.gradle.api.Task
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 
 class ReportTask extends DefaultTask {
 
     private Logger LOGGER = Logging.getLogger(ReportTask.class)
 
+    @OutputDirectory
+    File getOutputFolder(){
+        return new File(getProject().licenseReport.outputDir)
+    }
+
     @TaskAction
-     void generateReport() {
+    void generateReport() {
         LOGGER.info("Processing dependencies for project ${getProject().name}")
         LicenseReportExtension config = getProject().licenseReport
-        config.beforeExecute()
+        new File(config.outputDir).mkdirs()
         ProjectData data = new ProjectReader().read(getProject())
         LOGGER.info("Building report for project ${getProject().name}")
         config.renderer.render(data)
