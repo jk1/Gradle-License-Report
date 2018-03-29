eeb215c801863dc638ded83726183817cb3f4669
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2018-03-29 13:16:39 +0200

    throw exception when a configured is not resolvable, use canBeResolved

--- src/main/groovy/com/github/jk1/license/reader/ProjectReader.groovy
+++ src/main/groovy/com/github/jk1/license/reader/ProjectReader.groovy
@@ -1,5 +1,6 @@
 package com.github.jk1.license.reader
 
+import com.github.jk1.license.ConfigurationData
 import com.github.jk1.license.ProjectData
 import com.github.jk1.license.ReportTask
 import org.gradle.api.Project
@@ -7,9 +8,6 @@ import org.gradle.api.artifacts.Configuration
 import org.gradle.api.logging.Logger
 import org.gradle.api.logging.Logging
 
-/**
- * todo: rewrite me in an idiomatic way
- */
 class ProjectReader {
 
     private Logger LOGGER = Logging.getLogger(ReportTask.class)
@@ -24,38 +22,42 @@ class ProjectReader {
         configurationsToScan.addAll(getAllExtendedConfigurations(configurationsToScan))
 
         LOGGER.info("Configurations: " + configurationsToScan.join(','))
-        for (Configuration configuration : configurationsToScan) {
-            LOGGER.info("Reading configuration: " + configuration)
-            data.configurations.add(configurationReader.read(project, configuration))
-        }
+        data.configurations = readConfigurationData(configurationsToScan, project)
         return data
     }
 
-    private static Set<Configuration> getAllExtendedConfigurations(Set<Configuration> configurationsToScan) {
-        configurationsToScan.collect { it.extendsFrom }.flatten()
-    }
-
     private static Set<Configuration> findConfigurationsToScan(Project project) {
-        project.configurations.findAll { config ->
-            isConfigured(config, project) && isResolvable(config)
+        Set<Configuration> toScan = findConfigured(project)
+        Set<Configuration> unresolvable = findUnresolvable(toScan)
+        if (unresolvable) {
+            throw new UnresolvableConfigurationException("Unable to resolve configurations: $unresolvable")
+        }
+        if (!toScan) {
+            toScan = project.configurations.findAll { config -> isResolvable(config) }
+        }
+        toScan
+    }
+
+    private static Set<Configuration> getAllExtendedConfigurations(Set<Configuration> configurationsToScan) {
+        configurationsToScan.collect { it.extendsFrom }.flatten().findAll { config -> isResolvable(config) }
+    }
+
+    private List<ConfigurationData> readConfigurationData(Set<Configuration> configurationsToScan, Project project) {
+        configurationsToScan.collect { config ->
+            LOGGER.info("Reading configuration: " + config)
+            configurationReader.read(project, config)
         }
     }
 
-    private static boolean isConfigured(Configuration config, Project project) {
-        def configurationsToScan = project.licenseReport.configurations ?: project.configurations.collect {
-            it.name
-        }
-        config.name in configurationsToScan
+    private static Set<Configuration> findConfigured(Project project) {
+        project.configurations.findAll { config -> config.name in project.licenseReport.configurations }
+    }
+
+    private static Set<Configuration> findUnresolvable(Set<Configuration> toScan) {
+        toScan.findAll { config -> !isResolvable(config) }
     }
 
     private static boolean isResolvable(Configuration config) {
-        final def unresolvableConfigurations = ['pomResolver',
-                                                'apiElements',
-                                                'implementation',
-                                                'runtimeElements',
-                                                'runtimeOnly',
-                                                'testImplementation',
-                                                'testRuntimeOnly']
-        !(config.name in unresolvableConfigurations)
+        config.hasProperty("canBeResolved") && config.canBeResolved
     }
 }
---
+++ src/main/groovy/com/github/jk1/license/reader/UnresolvableConfigurationException.groovy
@@ -0,0 +1,8 @@
+package com.github.jk1.license.reader
+
+class UnresolvableConfigurationException extends Exception {
+
+    UnresolvableConfigurationException(String message) {
+        super(message)
+    }
+}
