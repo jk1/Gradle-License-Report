96b0409c5d21abcb9973fbded42d02365d5d39c1
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-03-31 16:28:16 +0800

    build: replace dead/unmaintained gradle-license-plugin with spotless
    Only being used for license header checks right now.

--- src/test/groovy/com/github/jk1/license/ReportTaskCachingSpec.groovy
+++ src/test/groovy/com/github/jk1/license/ReportTaskCachingSpec.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.jk1.license
 
 import org.gradle.testkit.runner.BuildResult
