a9d024a1d4e72c2eb235b357debdf41386c89395
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2026-04-01 22:34:59 +0800

    build: bump & pin GitHub actions for improved security posture (#385)
    * build: upgrade & pin GitHub actions for improved security posture
    
    Also introduces dependabot to do monthly PRs to update pieces. Should avoid touching the samples with this configuration.
    
    * chore: avoid duplicating wrapper for samples
    
    * build: replace dead/unmaintained gradle-license-plugin with spotless
    
    Only being used for license header checks right now.

--- src/main/groovy/com/github/jk1/license/filter/ExcludeDependenciesWithoutArtifactsFilter.groovy
+++ src/main/groovy/com/github/jk1/license/filter/ExcludeDependenciesWithoutArtifactsFilter.groovy
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.jk1.license.filter;
 
 import com.github.jk1.license.*;
